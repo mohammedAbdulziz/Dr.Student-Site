6efa0cafc0b6ea2f5579c886a233d8c3be4d43e0
mohammedAbdulziz <mohammedazizbuss@gmail.com>
2025-03-21 04:47:27 +0300

    created the doctor profile page

---
+++ Dashboards/scripts/doctor-profile-script.js
@@ -0,0 +1,5 @@
+// The name of the doctor
+const doctorNameTitle=document.querySelectorAll('main .doctor-info h1')[0];
+var doctorName="T. Shahlaa` Al-Swailim";
+doctorNameTitle.textContent= doctorName;
+
